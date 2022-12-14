2ac664fe1c003382e2b6e8c30d046b2e52412fa6
BocikPG <wojtekfarej@outlook.com>
2022-12-14 14:05:51 +0100

    going back to main menu on Esc

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,12 +10,22 @@ public class GameManager : MonoBehaviour
     public float Max_X;
     public Object EnemyPrefab;
 
+    [SerializeField] private ScenesController scenesController;
+
     //Unity methods
     void Start()
     {
         for(int i=0;i<1000;i++)
         {
             Object.Instantiate(EnemyPrefab,new Vector3(Random.Range(-Max_X,Max_X),Random.Range(-Max_Y,Max_Y),0),Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            scenesController.LoadMainMenuScene();
         }
     }
 
