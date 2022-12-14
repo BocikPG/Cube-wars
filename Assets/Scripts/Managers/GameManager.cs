98a0f52fe21f4fbf7c6a0fb4a1431b6b5fad7d9b
BocikPG <wojtekfarej@outlook.com>
2022-12-14 13:59:08 +0100

    increased spawning area + clean up

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //public/inspector
     public float Max_Y;
     public float Max_X;
     public Object EnemyPrefab;
+
+    //Unity methods
     void Start()
     {
         for(int i=0;i<1000;i++)
         {
-            Object.Instantiate(EnemyPrefab,new Vector3(Random.Range(-Max_X,Max_X),Random.Range(-Max_X,Max_X),0),Quaternion.identity);
+            Object.Instantiate(EnemyPrefab,new Vector3(Random.Range(-Max_X,Max_X),Random.Range(-Max_Y,Max_Y),0),Quaternion.identity);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
